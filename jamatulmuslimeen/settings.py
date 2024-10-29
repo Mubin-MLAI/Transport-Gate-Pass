2e2ff87746626d9be6c8cb9dd55e0b114bcb5716
Mubin Shafeek Shaikh <mubinshaikh@AD.NSEIT.COM>
2024-10-29 18:18:29 +0530

    changes in setting file

--- jamatulmuslimeen/settings.py
+++ jamatulmuslimeen/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'dummy key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mydjangoapp', '*']
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 
 # Application definition
