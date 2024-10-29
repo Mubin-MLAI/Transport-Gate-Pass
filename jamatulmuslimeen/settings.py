6eb0d4afbd9f87f70562aa17d8ee17117390e83c
Mubin Shafeek Shaikh <mubinshaikh@AD.NSEIT.COM>
2024-10-29 14:36:10 +0530

    db changes

--- jamatulmuslimeen/settings.py
+++ jamatulmuslimeen/settings.py
@@ -101,16 +101,16 @@ LOGIN_REDIRECT_URL = '/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jamatulmuslimeen',
-        'USER':'root', 
-        'PASSWORD':'root',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'jamatulmuslimeen',
+        # 'USER':'root', 
+        # 'PASSWORD':'root',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306'
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': 'G:/My Drive/Database/db.sqlite3',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
